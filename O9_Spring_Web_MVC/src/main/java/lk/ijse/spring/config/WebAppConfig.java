65316f8f3f1411ace939f2457e6178592a1c3f82
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-09-25 16:40:23 +0530

    web mvc

---
+++ O9_Spring_Web_MVC/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.spring.pojo")
+public class WebAppConfig {
+}
