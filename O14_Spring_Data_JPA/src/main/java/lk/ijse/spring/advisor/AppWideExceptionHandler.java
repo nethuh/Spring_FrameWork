71432a43998bb9da678e98f36d61f69163f6d0d5
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-10-09 11:56:42 +0530

    updated

--- O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
+++ O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
@@ -1,13 +1,16 @@
 package lk.ijse.spring.advisor;
 
 import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class AppWideExceptionHandler {
     @ExceptionHandler({RuntimeException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseUtil handleAllRuntimeException(RuntimeException e){
-        return new ResponseUtil("Error",e.getMessage(),null);
+        return new ResponseUtil("Error",e.getMessage(),"");
     }
 }
--- O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ O14_Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -4,6 +4,7 @@ import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.service.CustomerService;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,27 +17,35 @@ public class CustomerController {
     CustomerService service;
 
     @PostMapping
-    public void addCustomer(CustomerDTO dto){
+    public ResponseUtil addCustomer(CustomerDTO dto){
         service.addCustomer(dto);
+        return new ResponseUtil("OK","Successfully Added",dto);
     }
 
     @DeleteMapping(params = {"id"})
-    public void deleteCustomer(String id){
+    public ResponseUtil deleteCustomer(String id){
         service.deleteCustomer(id);
+        return new ResponseUtil("OK","Successfully Deleted",id);
+
     }
 
     @GetMapping
-    public List<CustomerDTO> getAllCustomer(){
-        return service.getAllCustomer();
+    public ResponseUtil getAllCustomer(){
+        return new ResponseUtil("OK","Successfully Loaded",service.getAllCustomer());
+
+
     }
 
     @GetMapping(params = {"id"})
-    public CustomerDTO findCustomer(String id){
-        return service.findCustomer(id);
+    public ResponseUtil findCustomer(String id){
+
+        return new ResponseUtil("OK","Successfully ",service.findCustomer(id));
+
     }
 
     @PutMapping
-    public void updateCustomer(@RequestBody CustomerDTO c){
+    public  ResponseUtil updateCustomer(@RequestBody CustomerDTO c){
         service.updateCustomer(c);
+        return new ResponseUtil("OK","Successfully Updated ",c);
     }
 }
